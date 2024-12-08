b2c4fe56f6426e0bc62a52c31f14d61324b71fcf
Shardul Janaskar <shardul2607@gmail.com>
2024-12-07 19:51:26 -0600

    implementing the training script

--- src/main.py
+++ src/main.py
@@ -1,4 +1,19 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
 from .models.resnet import resnet18
+from .data.dataloaders import get_imagenet_data
+
+def train_model(model, train_loader, val_loader, learning_rate=0.1):
+	device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
+	model = model.to(device)
+
+	criterion = nn.CrossEntropyLoss()
+	optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=5e-4)
+	scheduler = optim.lr_scheduler.StepLR(optimizer,step_size=30, gamma=0.1)
+
+	#Training loop to be implemented
 
 def main():
 	model = resnet18(num_classes=10, use_ws=True)
@@ -6,5 +21,4 @@ def main():
 
 
 if __name__ == "__main__":
-	main()
-
+	model = resnet18(num_classes=1000, use_ws=True)
